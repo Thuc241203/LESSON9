dc02b60f75d602b54d8593082c004e2d70892301
Thuc241203 <thucngo241203@gmail.com>
2023-02-05 20:49:40 +0700

    update challenge2

---
+++ challenge2/main.js
@@ -0,0 +1,47 @@
+
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+      [
+        'Neuer',
+        'Pavard',
+        'Martinez',
+        'Alaba',
+        'Davies',
+        'Kimmich',
+        'Goretzka',
+        'Coman',
+        'Muller',
+        'Gnarby',
+        'Lewandowski',
+      ],
+      [
+        'Burki',
+        'Schulz',
+        'Hummels',
+        'Akanji',
+        'Hakimi',
+        'Weigl',
+        'Witsel',
+        'Hazard',
+        'Brandt',
+        'Sancho',
+        'Gotze',
+      ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+      team1: 1.33,
+      x: 3.25,
+      team2: 6.5,
+    },
+};
+
+const log = console.log;
+
+// yêu cầu 1:
+for(const [i, player] of game.scored.entries())
+log(`Goal ${i + 1}: ${player}`);
