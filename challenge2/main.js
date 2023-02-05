8baf5ab0bd1018e8adb608386f3ef6110e89f814
Thuc241203 <thucngo241203@gmail.com>
2023-02-05 21:13:23 +0700

    update js

--- challenge2/main.js
+++ challenge2/main.js
@@ -47,6 +47,14 @@ for(const [i, player] of game.scored.entries())
     log(`Goal ${i + 1}: ${player}`);
 
 // yêu cầu 2:
+const odds = Object.values(game.odds);
 let average = 0;
-for (const odd of Object.values(game.odds))
+for (const odd of odds) average += odd;
     average /= odds.length;
+    log(average);
+
+// yêu cầu 3:
+for(const [team,odd] of Object.entries(game.odds)){
+    const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
+    log(`Odd of ${teamStr} ${odd}`);
+};
