a44a6186bca61e5190b0add41278c2e65e07f1d1
Thuc241203 <thucngo241203@gmail.com>
2023-02-05 21:49:52 +0700

    update js

--- challenge4/main.js
+++ challenge4/main.js
@@ -1,3 +1,16 @@
+const log = console.log;
 
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
+
+const text = document.querySelector('textarea').value;
+
+document.querySelector('button').addEventListener
+('click', function () {
+    const text =document.querySelector('textarea').value;
+    const rows = text.split('\n');
+    log(rows);
+    for (const row in rows) {
+        row.split('_');
+    }
+});
